effc734094c914cb171d71577ee7835654109846
ThommysArt <keabou.thomson@ictuniversity.edu.cm>
2025-01-21 22:40:56 +0100

    updated navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,44 +1,37 @@
 "use client"
 
 import { cn } from '@/lib/utils'
-import { CalendarIcon, EnvelopeClosedIcon, GearIcon, HomeIcon, PersonIcon } from '@radix-ui/react-icons'
+import { CalendarIcon, EnvelopeClosedIcon, GearIcon, HomeIcon, Pencil2Icon, PersonIcon } from '@radix-ui/react-icons'
 import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
+import { Button } from './ui/button'
 
 export const Navbar = () => {
     const pathname = usePathname()
     const router = useRouter()
+
+    const sections = [
+        {title: "Projects", icon: HomeIcon, path: "/home/projects"},
+        {title: "Calendar", icon: CalendarIcon, path: "/home/calendar"},
+        // {title: "Emails", icon: EnvelopeClosedIcon, path: "/home/emails"},
+        {title: "Notes", icon: Pencil2Icon, path: "/home/notes"},
+        {title: "Orgs", icon: PersonIcon, path: "/home/organisations"},
+        {title: "Settings", icon: GearIcon, path: "/home/settings"},
+    ]
   return (
-    <nav className='flex flex-row justify-between items-center px-3'>
-        <div className={cn("flex flex-col gap-1 text-sm hover:text-primary items-center justify-center", pathname.includes("/home/projects") ? "text-primary":"text-muted-foreground")}
-            onClick={()=>router.push("/home/projects")}
-        >
-            <HomeIcon className="w-6 h-6" />
-            Projects
-        </div>
-        <div className={cn("flex flex-col gap-1 text-sm hover:text-primary items-center justify-center", pathname.includes("/home/calendar") ? "text-primary":"text-muted-foreground")}
-            onClick={()=>router.push("/home/calendar")}
-        >
-            <CalendarIcon className="w-6 h-6" />
-            Calendar
-        </div>
-        <div className={cn("flex flex-col gap-1 text-sm hover:text-primary items-center justify-center", pathname.includes("/home/emails") ? "text-primary":"text-muted-foreground")}
-            onClick={()=>router.push("/home/emails")}
-        >
-            <EnvelopeClosedIcon className="w-6 h-6" />
-            Emails
-        </div>
-        <div className={cn("flex flex-col gap-1 text-sm hover:text-primary items-center justify-center", pathname.includes("/home/organisations") ? "text-primary":"text-muted-foreground")}
-            onClick={()=>router.push("/home/organisations")}
-        >
-            <PersonIcon className="w-6 h-6" />
-            Orgs
-        </div>
-        <div className={cn("flex flex-col gap-1 text-sm hover:text-primary items-center justify-center", pathname.includes("/home/settings") ? "text-primary":"text-muted-foreground")}
-            onClick={()=>router.push("/home/settings")}
-        >
-            <GearIcon className="w-6 h-6" />
-            Settings
+    <nav className="flex w-full justify-center ">
+        <div className="fixed bottom-5 flex z-50 border rounded-full mx-3 w-fit h-16 bg-muted/20 items-center justify-center backdrop-blur-sm">
+            {sections.map((section, idx) => (
+                <Button
+                    key={idx}
+                    onClick={() => router.push(section.path)}
+                    variant={pathname.includes(section.path) ? 'default' : 'ghost'}
+                    size="icon"
+                    className="rounded-full w-full h-full p-4"
+                    >
+                    <section.icon className="w-8 h-8" />
+                </Button>
+            ))}
         </div>
     </nav>
   )
