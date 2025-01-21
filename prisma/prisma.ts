86353cba2c317a23615b418d33719cbf4b63f6ce
ThommysArt <keabou.thomson@ictuniversity.edu.cm>
2025-01-22 00:00:39 +0100

    rm neon polling

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,12 +1,10 @@
 
-import { Pool } from "@neondatabase/serverless";
+import { Pool, neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
-  const adapter = new PrismaNeon(neon);
-  return new PrismaClient({ adapter });
+  return new PrismaClient();
 };
 
 declare global {
